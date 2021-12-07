146def0f3077e7b386fcff0d13223db26ae3fefa
Alberto Rosales <arosales7@ucol.mx>
2021-12-07 17:19:13 -0600

    BasedeDatos

---
+++ infodb.js
@@ -0,0 +1,30 @@
+const Recetas = [
+    // Posicion 1
+    {
+        Name: "Pepperoni Pizza Pockets",
+        Image:"https://placeralplato.com/files/2016/01/Pizza-con-pepperoni-640x480.jpg?width=1200&enable=upscale",
+        Servings: "3",
+        Ingredents= [
+            {PuffPastry:"1 sheets"},
+            {Pepperoni:"15 slices"},
+            {MarinadaSauce:"1 cup"},
+            {SherddedMozzarellaCheese:"1/2 cup"},
+            {Egg:"1"}
+        ]
+        
+    },
+    
+    // Posicion 2
+    {
+        Name: "Meatball Sub Bake",
+        Image:"https://www.julieseatsandtreats.com/wp-content/uploads/2012/09/Easy-Meatball-Sub-Casserole-1024x682.jpg",
+        Servings: "3",
+        Ingredents= [
+            {RefrigeredBiscuit:"1 tube"},
+            {FrozenMiniMeatballs:"24"},
+            {MarinadaSauce:"1 jar"},
+            {SherddedMozzarellaCheese:"2 cup"}
+        ]
+        
+    }
+]
