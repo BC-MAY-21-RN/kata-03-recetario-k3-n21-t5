b88bca1c073e6b26c2d4f49a553e6af0071fe301
Angel Abraham Flores Ramirez <60322727+AangelF@users.noreply.github.com>
2021-12-10 18:19:58 -0600

    Update IngredientsStyles.js

--- ExpoProject/src/components/IngredientsStyles.js
+++ ExpoProject/src/components/IngredientsStyles.js
@@ -1,12 +1,12 @@
 import { StyleSheet } from 'react-native'
 
 export const Styles = StyleSheet.create({
-    contianer:{
+    container:{
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'space-around',
+        justifyContent: 'space-between',
         borderBottomWidth: 1,
-        borderBottomColor: '#fff',
+        borderBottomColor: '#444444',
         marginTop: 4
     },
     name:{
